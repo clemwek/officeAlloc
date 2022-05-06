79fecc0427cec579f83620f5d7cde4d478216c9f
Clement <clemwek@gmail.com>
2022-05-06 18:24:34 +0300

    Initial Commit

---
+++ officeSpaceAllocation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  officeSpaceAllocation
+//
+//  Created by Clement  Wekesa on 06/05/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
